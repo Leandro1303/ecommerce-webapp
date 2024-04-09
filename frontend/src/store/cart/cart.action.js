2cf14fd9c821eb10ddd3d2cd1969f8c852e04d3a
Tiff Jam <tiffjamofficial@gmail.com>
2024-04-09 16:45:35 -0400

    solucion de bug del cart gracias a Hector y Leandro

--- frontend/src/store/cart/cart.action.js
+++ frontend/src/store/cart/cart.action.js
@@ -1,14 +1,14 @@
 import { createAction } from "../../utils/reducer/reducer.utils.js";
 import { CART_ACTION_TYPES } from "./cart.types.js";
 
-export const addCartItem = (cartItems, productToAdd, quantity = 1) => {
+const addCartItem = (cartItems, productToAdd, quantity = 1) => {
     const existingCartItem = cartItems.find(
-        (cartItem) => cartItem.id === productToAdd.id
+        (cartItem) => cartItem._id === productToAdd._id
     );
 
     if (existingCartItem) {
         return cartItems.map((cartItem) =>
-            cartItem.id === productToAdd.id
+            cartItem._id === productToAdd._id
             ? { ...cartItem, quantity: cartItem.quantity + quantity }
             : cartItem
         );
@@ -20,24 +20,24 @@ export const addCartItem = (cartItems, productToAdd, quantity = 1) => {
 const removeCartItem = (cartItems, cartItemToRemove) => {
     //find the cart item to remove
     const existingCartItem = cartItems.find(
-        (cartItem) => cartItem.id === cartItemToRemove.id
+        (cartItem) => cartItem._id === cartItemToRemove._id
     );
 
     //chack if quantity is equal to 1, if it is remove that item from the cart
     if (existingCartItem.quantity === 1) {
-        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+        return cartItems.filter(cartItem => cartItem._id !== cartItemToRemove._id);
     }
 
     //return back cartitems with matching cart item with reduced quantity
     return cartItems.map((cartItem) =>
-            cartItem.id === cartItemToRemove.id
+            cartItem._id === cartItemToRemove._id
             ? { ...cartItem, quantity: cartItem.quantity - 1 }
             : cartItem
     );
 }
 
 const clearCartItem = (cartItems, cartItemToClear) =>
-    cartItems.filter(cartItem => cartItem.id !== cartItemToClear.id);
+    cartItems.filter(cartItem => cartItem._id !== cartItemToClear._id);
 
 export const setIsCartOpen = (boolean) =>
     createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, boolean);
